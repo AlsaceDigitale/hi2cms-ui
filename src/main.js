d5529202a92c87d6a5e208c1c1a612fc0e249f42
sabesso <sabesso@hotmail.com>
2020-06-08 15:32:33 +0000

    Adding some filters for the date & hours

--- src/main.js
+++ src/main.js
@@ -1,8 +1,41 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import moment from "moment";
 
 Vue.config.productionTip = false;
+
+Vue.filter('frStartTimeFormat', function (value) {
+  moment.locale('fr');
+  return moment(value).format("dddd D MMMM YYYY [de] H[h]mm [à] ");
+});
+
+Vue.filter('frStartTimeFormat1', function (value) {
+  moment.locale('fr');
+  return moment(value).format("ddd D MMM");
+});
+
+Vue.filter('frStartTimeFormat2', function (value) {
+  moment.locale('fr');
+  return moment(value).format("H[h]mm");
+});
+
+Vue.filter('enStartTimeFormat', function (value) {
+  moment.locale('en');
+  return moment(value).format("dddd D MMMM YYYY [from] H[h]mm [to] ");
+});
+
+Vue.filter('endTimeFormat', function (value) {
+  return moment(value).format("H[h]mm");
+});
+// Vue.filter('enEndTimeFormat', function (value) {
+//   return moment(value).format("H[h]mm");
+// });
+
+Vue.filter('capitalize', function (value) {
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+});
 
 new Vue({
   router,
