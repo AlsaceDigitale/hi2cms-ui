149bcda161ec6da888c03a013204b48853992858
sabesso <sabesso@hotmail.com>
2020-06-22 23:48:42 +0000

    Create a new component for the section About

---
+++ src/components/About.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container">
+        <h2 class="section-heading text-center">A Propos</h2>
+        <div class="about-row row">
+          <!-- DESCRIPTION IMAGE -->
+          <div class="about-image col-md-6" data-animation="fadeIn">
+            <img alt="a propos" src="images/contents/a-propos.jpg" />
+          </div>
+          <!-- DESCRIPTION TEXT -->
+          <div class="about-text col-md-6">
+            <p class="lead" style="font-size: 18px;padding-top: 0;">
+              Le
+              <strong>Hacking Industry Camp par
+                <a href="http://alsacedigitale.org/" target="_blank">Alsace Digitale</a></strong>
+              est un hackathon dédié à l’industrie, un évènement destiné à
+              bousculer les idées et à faire émerger de nouveaux projets. Des
+              équipes aux divers talents s’affrontent pendant tout un week-end
+              pour concevoir des prototypes d’applications, d’objets connectés…
+              C’est un espace-temps où les communautés se croisent et
+              fourmillent d’idées avec pour objectif l’innovation !
+            </p>
+            <ul class="icon-list">
+              <li>
+                <span class="icon-list-icon" data-animation="bounceIn"><i class="fa fa-hand-o-up" aria-hidden="true"></i></span>
+                <h4 class="icon-list-title">Pointer les projets émergents</h4>
+                <p>
+                  54 h pour participer à l’aventure Usine 4.0. Déplacer
+                  l’industrie dans ses fondements, l’interroger sur ses process,
+                  la rendre plus agile, plus inventive.
+                </p>
+              </li>
+              <li>
+                <span class="icon-list-icon" data-animation="bounceIn"><i class="fa fa-heart" aria-hidden="true"></i></span>
+                <h4 class="icon-list-title">Génération Même pas peur</h4>
+                <p>
+                  54 h pour innover à plusieurs. Un concentré d’énergie créative
+                  de tous bords : développeurs, ingénieurs, designers,
+                  bricoleurs, utilisateurs, marketeurs…
+                </p>
+              </li>
+              <li>
+                <span class="icon-list-icon" data-animation="bounceIn"><i class="fa fa-lightbulb-o" aria-hidden="true"></i></span>
+                <h4 class="icon-list-title">
+                  Idées d'aujourd'hui, Entreprises de demain
+                </h4>
+                <p>
+                  54 h pour démultiplier les idées, les faire émerger, les
+                  concrétiser et donner naissance à des projets innovants qui se
+                  poursuivront, soit par la création de start up, soit au sein
+                  d’entreprises existantes.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
