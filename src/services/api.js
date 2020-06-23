e15e32f68ece3f330c4bd905038d4567224e3ba6
sabesso <sabesso@hotmail.com>
2020-06-23 00:02:22 +0000

    Adding some function for the API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-let baseURL = process.env["VUE_APP_API_ROOT"] || "http://192.168.0.34:1337/";
-let mediaURL = process.env["VUE_APP_MEDIA_ROOT"] || "http://192.168.0.34:1337";
+let baseURL = process.env["VUE_APP_API_ROOT"] || "http://192.168.0.19:1337/";
+let mediaURL = process.env["VUE_APP_MEDIA_ROOT"] || "http://192.168.0.19:1337";
 
 export const HTTP = axios.create({
     baseURL: baseURL
@@ -27,9 +27,24 @@ export default {
         return HTTP.get("/announces");
     },
     getProcesses: function () {
-        return HTTP.get("/processes");
+        return HTTP.get("/processes?_sort=priority:ASC");
     },
+    getResults: function () {
+        return HTTP.get("/results");
+    },
+    getQuestions: function () {
+        return HTTP.get("/questions");
+    },
+    getPartnerCategories: function () {
+        return HTTP.get("/partnercategories");
+    },
+    getZones: function () {
+        return HTTP.get("/zones");
+    },
+    // getKeySteps: function () {
+    //     return HTTP.get("/keysteps");
+    // },
     getMediaRoot: function () {
         return mediaURL;
-    }   
+    }
 };
