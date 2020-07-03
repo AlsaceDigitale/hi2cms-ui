7de60921d338c7ead79a1df64119546cdc0df71f
sabesso <sabesso@hotmail.com>
2020-07-03 09:39:36 +0000

    Create a new components for the map

---
+++ src/components/Map.vue
@@ -0,0 +1,68 @@
+<template>
+
+  <div style="height: 500px; width: 100%">
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+      <l-marker :lat-lng="withTooltip">
+        <l-tooltip :options="{ permanent: true, interactive: true }">
+          <div>
+            Télécom Physique Strasbourg
+          </div>
+        </l-tooltip>
+      </l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LTooltip } from "vue2-leaflet";
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 17,
+      center: latLng(48.526102, 7.737071),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
+      withTooltip: latLng(48.526102, 7.737071),
+      mapOptions: {
+        zoomSnap: 0.5
+      }
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    }
+  }
+};
+</script>
