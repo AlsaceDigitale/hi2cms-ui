6d8364e8d522bc8b990bfd2fdc93b02da2d84b24
sabesso <sabesso@hotmail.com>
2020-07-07 07:27:22 +0000

    Define the infos of Map through Props

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div style="height: 500px; width: 100%">
     <l-map
       :zoom="zoom"
@@ -8,10 +7,7 @@
       @update:center="centerUpdate"
       @update:zoom="zoomUpdate"
     >
-      <l-tile-layer
-        :url="url"
-        :attribution="attribution"
-      />
+      <l-tile-layer :url="url" :attribution="attribution" />
       <l-marker :lat-lng="withTooltip">
         <l-tooltip :options="{ permanent: true, interactive: true }">
           <div>
@@ -26,17 +22,20 @@
 <script>
 import { latLng } from "leaflet";
 import { LMap, LTileLayer, LMarker, LTooltip } from "vue2-leaflet";
-import { Icon } from 'leaflet';
+import { Icon } from "leaflet";
 
 delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
 });
 
 export default {
   name: "Map",
+  props: {
+    location: Object
+  },
   components: {
     LMap,
     LTileLayer,
@@ -47,7 +46,7 @@ export default {
     return {
       zoom: 17,
       center: latLng(48.526102, 7.737071),
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors',
       withTooltip: latLng(48.526102, 7.737071),
