0f7bfa29e82f5aee0f555a6a4cfb651ccf404966
sabesso <sabesso@hotmail.com>
2021-01-19 19:23:21 +0100

    Adding the Special zone to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -38,6 +38,7 @@
           <li v-if="planning"><a href="#planning">Planning</a></li>
           <li v-if="result"><a href="#winners">Résultats</a></li>
           <li v-if="partner"><a href="#sponsors">Partenaires</a></li>
+          <li v-if="specialzone"><a href="#specialZone">Chaire IA</a></li>
           <li v-if="question"><a href="#faq">FAQ</a></li>
           <!-- HEADER ACTION BUTTON -->
           <li class="header-action-button">
@@ -99,7 +100,8 @@ export default {
     planning: Boolean,
     result: Boolean,
     partner: Boolean,
-    question: Boolean
+    question: Boolean,
+    specialzone: Boolean
   }
 };
 </script>
