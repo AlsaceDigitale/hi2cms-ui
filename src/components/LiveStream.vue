691226bdfe36acbf31753e27444fa0238942883d
sabesso <sabesso@hotmail.com>
2021-02-05 00:54:33 +0100

    Create new component LiveStream for the pageLive

---
+++ src/components/LiveStream.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h2 style="margin-top: 50px; font-weight: bold">Hacking Industry e-Camp en ligne</h2>
+    <div class="hero-content">
+        <iframe
+            src="https://player.twitch.tv/?channel=alsacedigitale&parent=hackingindustry.camp"
+            height="315"
+            width="560"
+            allowfullscreen=true>
+        </iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LiveStream",
+};
+</script>
+
+<style scoped>
+/* @media screen and (min-width: 992px) and (max-width: 1200px){
+  .divMargin {
+    margin-top: 120px;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .liMargin {
+    margin-bottom: 20px;
+  }
+  .divMargin {
+    margin-top: 160px;
+  }
+} */
+
+</style>
