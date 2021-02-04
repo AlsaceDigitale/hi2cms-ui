bab432630fe4be1f001b32e850246a90c185f914
sabesso <sabesso@hotmail.com>
2021-02-05 00:46:49 +0100

    Adding new Live route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Live from "../views/Live.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/live",
+    name: "Live",
+    component: Live
   },
   {
     path: "/about",
