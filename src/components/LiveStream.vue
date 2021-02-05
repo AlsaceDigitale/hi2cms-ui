5afa678394498c4c6d971ab851bf648f5cd8424e
Yannick JOST <yannick.jost@alsacedigitale.org>
2021-02-05 13:25:07 +0100

    fix twitch embed

--- src/components/LiveStream.vue
+++ src/components/LiveStream.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="container">
-    <h2 style="margin-top: 50px; font-weight: bold">Hacking Industry e-Camp en ligne</h2>
-    <div class="hero-content">
-        <iframe
-            src="https://player.twitch.tv/?channel=alsacedigitale&parent=hackingindustry.camp"
-            height="315"
-            width="560"
-            allowfullscreen=true>
-        </iframe>
+  <div>
+    <!-- Add a placeholder for the Twitch embed -->
+    <div id="twitch-embed"></div>
+
+    <!-- Load the Twitch embed script -->
+    <script src="https://embed.twitch.tv/embed/v1.js"></script>
+
+      <!-- Create a Twitch.Embed object that will render within the "twitch-embed" root element. -->
+      <script type="text/javascript">
+        new Twitch.Embed("twitch-embed", {
+          width: 854,
+          height: 480,
+          channel: "alsacedigitale",
+          // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+          parent: ["hackingindustry.camp", "hi2cms-dev.netlify.app/"]
+        });
+      </script>
     </div>
-  </div>
 </template>
 
 <script>
