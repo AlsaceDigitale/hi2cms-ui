23831f74e462df61f1f6341894d713431fb0fcd2
Yannick JOST <yannick.jost@alsacedigitale.org>
2021-02-05 15:21:45 +0100

    remove live

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,6 @@ const routes = [
     component: Home
   },
   {
-    path: "/live",
-    name: "Live",
-    component: Live
-  },
-  {
     path: "/about",
     name: "About",
     // route level code-splitting
