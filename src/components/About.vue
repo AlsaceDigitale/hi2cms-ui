299b898724eab4f599cd0ee21b748bd6490c3b7e
sabesso <sabesso@hotmail.com>
2021-09-15 08:57:58 +0200

    delete link of the button Je participe au Hachathon

--- src/components/About.vue
+++ src/components/About.vue
@@ -94,7 +94,7 @@
             <div class="hero-buttons col-md-12 text-center">
               <a
                 class="btn btn-lg btn-primary"
-                href="https://www.helloasso.com/associations/alsace-digitale/evenements/hacking-industry-camp-1"
+                href="#"
                 style="margin-top: 20px;"
                 target="_blank"
                 >Je participe au hackathon !</a
