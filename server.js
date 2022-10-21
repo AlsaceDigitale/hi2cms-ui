27ac347f41f0d94fc11009d674477427f7a08e0d
Yannick Jost <yannick@yjost.com>
2022-10-21 10:58:15 +0200

    fix server.js

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT
+var public_folder = __dirname + '/dist'
 
 console.info('Listening on port '+port)
+console.info('Serving files from '+public_folder)
 
 //setting middleware
-app.use(express.static(__dirname + 'dist')); //Serves resources from public folder
+app.use(express.static(public_folder)); //Serves resources from public folder
 
 
 var server = app.listen(port);
